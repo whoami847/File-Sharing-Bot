7e42f487694ff32a32451805c308d5d4da1061c0
whoami847 <mrbeastbangla069@gmail.com>
2025-04-14 11:20:15 +0600

    Update helper_func.py

--- helper_func.py
+++ helper_func.py
@@ -8,10 +8,10 @@ async def check_force_sub(client, message):
     try:
         user_id = message.from_user.id
         member = await client.get_chat_member(FORCE_SUB_CHANNEL, user_id)
-        
+
         if member.status in ("kicked", "banned"):
             return False  # User is banned from the channel
-        
+
         return True  # User is a member
 
     except UserNotParticipant:
